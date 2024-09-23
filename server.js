f65e2abb3b45a602aa0f6dc7ec4e47eab01d253d
kalleman1 <kaper1@live.dk>
2024-09-23 17:47:36 +0200

    Added topic exchange

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
 import express from 'express';
 import amqp from 'amqplib';
+import cors from 'cors';
 
 //Instancierer en express server, så vi kan bruge den til at oprette endpoints
 const app = express();
 
-//Opret forbindelse til RabbitMQ
+//Cors for at sikre, at jeg kan modtage requests fra frontend
+app.use(cors());
+app.use(express.json());
+
 let channel;
+const exchangeName = 'tourExchange';
+
 const connectRabbitMQ = async () => {
-    const connection = await amqp.connect('amqp://rabbitmq');
+    const connection = await amqp.connect('amqp://localhost');
     //Opretter en channel på rabbitMQ
     channel = await connection.createChannel();
     //Opretter en queue på rabbitMQ med navnet 'tourbookings'
-    await channel.assertQueue('tourbookings');
+    await channel.assertExchange(exchangeName, 'topic', { durable: false });
 }
 
 connectRabbitMQ().catch(console.error);
 
 //Opsætning af endpoint for at modtage tour objektet fra frontend og sende til rabbitMQ. 
 app.post('/create-tour', async (req, res) => {
+    console.log('Recieved tour request from frontend: ', req.body);
+
     //Tager fat i tour objektet fra request body fra frontend
     const tour = req.body;
 
-    //Forsøg at omdanne denne til en json string og send den til rabbitMQ, ellers send en fejlmeddelelse til frontend
+    const { isBooking } = tour;
+
+    const routingKey = isBooking ? 'tour.booking' : 'tour.cancel';
+    console.log('Routing Key: ', routingKey)
+    
     try {
-        await channel.sendToQueue('tourbookings', Buffer.from(JSON.stringify(tour)));
-        res.sendStatus(200).send('Tour successfully sent to RabbitMQ');
+        await channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(tour)));
+        res.sendStatus(200);
     } catch (error) {
         console.log('Error sending tour to RabbitMQ', error);
-        res.status(500).send('Error sending tour to RabbitMQ');
+        res.sendStatus(500); 
     }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3001, () => console.log('Server running on port 3001'));
